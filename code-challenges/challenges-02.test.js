d393bfc181739e4a176de66e5cfed0d924637fc9
overholtk <overholtkaya@gmail.com>
2020-11-03 14:39:20 -0800

    completed stretch goals

--- code-challenges/challenges-02.test.js
+++ code-challenges/challenges-02.test.js
@@ -79,7 +79,9 @@ console.log(people[1].isAuthor) prints true
 ------------------------------------------------------------------------------------------------ */
 
 const setStatusAsAuthor = (people) => {
-  // Solution code here...
+  people.forEach ((val, idx) => {
+    val.isAuthor = true;
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -98,7 +100,9 @@ console.log(a) prints [1, 2, 3, 4]
 ------------------------------------------------------------------------------------------------ */
 
 const append = (arr1, arr2) => {
-  // Solution code here...
+  arr2.forEach ((val, idx) => {
+    arr1.push(val);
+  });
 
 };
 
@@ -147,7 +151,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should add a property to every object in an array', () => {
     const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
     setStatusAsAuthor(a);
@@ -158,7 +162,7 @@ xdescribe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
